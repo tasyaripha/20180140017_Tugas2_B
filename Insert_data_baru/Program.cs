3f870e750b79158f06a27b9bb9c712d6a2e3596c
TasyaRipha <raykaagustasya16@gmail.com>
2022-03-24 18:11:22 +0700

    Add project files.

---
+++ Insert_data_baru/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Insert_data_baru
+{
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            new Program().InsertData();
+        }
+
+        private void InsertData()
+        {
+            SqlConnection con = null;
+        try
+            {
+                con = new SqlConnection("data soruce=DESKTOP-CCBP8LR; database=ProdiTI;Integrated Security = TRUE");
+                con.Open();
+
+                SqlCommand cm = new SqlCommand("insert into Mahasiswa_Coba (NIM, Nama, Alamat, Jenis_Kelamin) values('2019010099','Ronald','Yogyakarta', 'L')", con);
+                cm.ExecuteNonQuery();
+
+                Console.WriteLine("Sukses Menambahkan Data");
+                Console.ReadKey();
+            }   catch(Exception e)
+            {
+                Console.WriteLine("Gagal Menambahkan Data. " + e);
+                Console.ReadKey();
+            }   finally
+            {
+                con.Close();
+            }
+        }  
+
+        }
+    }
+
